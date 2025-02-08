88dc511fde92324152025b31c9b068f64ab99e33
pix <tpxsky@163.com>
2025-02-09 00:58:05 +0800

    <add:> add variable test

---
+++ test_val.lua
@@ -0,0 +1,17 @@
+a = 1
+print(a)
+local b = 2
+print(b)
+print(c)
+a, b = 11, 12
+print(a)
+print(b)
+b = 0x11
+c = 2e10
+print("a = " .. a)
+print("b = " .. b)
+print("c = " .. c)
+
+print(a + b)
+print(10 ^ 5)
+print(1 << 3)
